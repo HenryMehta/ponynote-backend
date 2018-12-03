ab3a26eafe74b70b70715baaf300750d930010c0
HenryMehta <hjsmehta@gmail.com>
2018-12-03 15:50:40 +0000

    allow owner to be blank

--- notes/models.py
+++ notes/models.py
@@ -6,7 +6,13 @@ from django.contrib.auth.models import User
 class Note(models.Model):
     text = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
-    owner = models.ForeignKey(User, related_name="notes",
-                              on_delete=models.CASCADE, null=True)
+    owner = models.ForeignKey(
+        User, 
+        related_name="notes",
+        on_delete=models.CASCADE, 
+        null=True,
+        blank=True
+        )
+        
     def __str__(self):
         return self.text
