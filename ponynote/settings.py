36c3e0d81bf890b8d6956d815f4599f62bd35810
HenryMehta <hjsmehta@gmail.com>
2018-12-18 15:40:00 +0000

    added authentication token

--- ponynote/settings.py
+++ ponynote/settings.py
@@ -42,9 +42,19 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'notes',
     'rest_framework',
+    'rest_framework.authtoken',
     'knox',
     'ponynote'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
